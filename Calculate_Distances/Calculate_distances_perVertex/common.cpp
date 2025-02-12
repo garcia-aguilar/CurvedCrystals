8936c8dad177e5988dc21f0ed643178e7d151ea9
garcia-aguilar <i.r.garciaaguilar@tudelft.nl>
2025-02-12 23:36:48 +0100

    Uploaded geodesic distance calculation for single vertex

---
+++ Calculate_Distances/Calculate_distances_perVertex/common.cpp
@@ -0,0 +1,141 @@
+#include "common.h"
+
+// *** Mesh variables used throughout the code **
+//long num_of_meshpoint;
+//Vertex vertex[MAX_SIZE];
+double total_area;
+double total_volume;
+double dl_min;              // minimal edge length in the mesh -> DT_max calculation
+
+// *** File variables used throughout the code **
+FILE *f_ou;
+
+// *** Integration parameters read from input **
+double init_stress;                 // initial total pre-stress
+double phi_range;                   // set the initial stress at each point randomly within this range
+long seed;                          // for random fx to set initial stress value
+//long num_of_iteration;
+//double run_time;                    // integration time step set accordingly        // change to read DT**
+double time_step;                   // integration time step
+int integration_method;             // 1. Euler   2. Runge-Kutta (2 steps)  3. Runge-Kutta (4 steps) -> [bug]
+double convergence_stop;            // stop integration when num_iterations completed or reaching this error
+
+// *** integration variables used throughout the code **
+double convergence;
+
+
+
+/*******************************************************************/
+/*******************************************************************/
+
+// **** Common functions   **
+
+/************************************************************************/
+// 3.6.3. progress_bar:
+//          print on-screen percentage bar of a process at t from total
+void progress_bar(long t, long total)
+{
+    long i, ticks, percent, num_of_ticks=20;
+	char progress[32];
+
+	percent = 100*t/total;
+	ticks = num_of_ticks*percent/100;
+
+	for (i=0; i<20; i++){
+		progress[i] = ((i<=ticks)?'#':' ');
+	}
+	progress[20] = '\0';
+
+	printf("Progress: [%s] %ld%%\r",progress,percent);
+	fflush(stdout);
+}
+/****************************************************/
+// 3.6.4. progress_bar_on_ou:
+//          print on ou.dat percentage bar of a process at t from total
+int progress_bar_on_ou(long t, long total, int prog_count)
+{
+	float percent = 100*t/total;
+
+	int current_count = percent/10;
+	if (current_count > prog_count)
+	{
+        fprintf(f_ou," %d .",10-current_count);
+        fflush(f_ou);
+    }
+    return current_count;
+
+}
+/************************************************************************/
+/*******************************************************************/
+// 6. end:
+//      Get end times and print (on screen and in ou.dat) final outputs
+/*
+void end(time_t t0, time_t t1)
+{
+    CPU_Time cpu_time_total,cpu_time_geo,cpu_time_run;
+    time_t t2;
+	time(&t2);
+  	get_time(t2-t1,&cpu_time_run);                  // 6.1
+    get_time(t2-t0, &cpu_time_total);
+    get_time(t1-t0, &cpu_time_geo);
+
+	export_conf(-1,-1);         //save last.dat file        // 5.2.2 in run_field_integration
+
+    double total_stretch=0;
+    for(long i=0; i<num_of_meshpoint; i++){
+        total_stretch += vertex[i].phi*vertex[i].phi*vertex[i].area;
+    }
+
+  // Write final values of interest in ou.dat
+    fprintf(f_ou, "\n<<< Final Variables >>>\n");
+    fprintf(f_ou,"TotalStretch  %.8lG\n", total_stretch);
+    fprintf(f_ou,"RHS^2  %.4lG\n", convergence);
+    /*fprintf(f_ou,"CPU_Geometry_Time %ld:%ld:%ld:%ld\n",
+     cpu_time_geo.d,cpu_time_geo.h,
+	 cpu_time_geo.m,cpu_time_geo.s);*/  //Print out before
+ /*
+    fprintf(f_ou,"CPU_Run_Time %ld:%ld:%ld:%ld\n",
+     cpu_time_run.d,cpu_time_run.h,
+	 cpu_time_run.m,cpu_time_run.s);
+    fprintf(f_ou,"CPU_Total_Time %ld:%ld:%ld:%ld\n",
+	 cpu_time_total.d,cpu_time_total.h,
+	 cpu_time_total.m,cpu_time_total.s);
+
+  // Print final screen output
+    printf("\n\n ****** End Of Integration Steps ******\n");
+    printf("\tTotalStretch  %.8lG\n", total_stretch);
+    printf("\tRHS^2  %.4lG\n", convergence);
+    printf("\n CPU_Run_Time %ld:%ld:%ld:%ld\n",
+     cpu_time_run.d,cpu_time_run.h,
+	 cpu_time_run.m,cpu_time_run.s);
+	 printf("-------------------------------\n");
+    printf(" CPU_Total_Time %ld:%ld:%ld:%ld\n\n",
+     cpu_time_total.d,cpu_time_total.h,cpu_time_total.m,cpu_time_total.s);
+
+    /*    UNCOMMENT to write .m (mathematica) file
+	f_ou = fopen("last.m","w");
+	export_graphic_complex(f_ou);
+	fclose(f_ou);
+	*/
+
+
+/*
+}*/
+/*******************************************************************/
+// 6. end:
+//    write CPU time in hour:minute:second format
+void get_time(time_t t, CPU_Time *cpu_time)
+{
+  long sec_in_day, sec_in_ora, sec_in_min;
+
+  sec_in_day = 86400;
+  sec_in_ora = 3600;
+  sec_in_min = 60;
+
+  cpu_time->d = t/sec_in_day; t = t%sec_in_day;
+  cpu_time->h = t/sec_in_ora; t = t%sec_in_ora;
+  cpu_time->m = t/sec_in_min; t = t%sec_in_min;
+  cpu_time->s = t;
+}
+/*******************************************************************/
+/************************************************************************/
---
+++ Calculate_Distances/Calculate_distances_perVertex/common.h
@@ -0,0 +1,178 @@
+/// Declarations of globals and common objects
+
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+///// geodesic_algorithm  ---       // include only in init_geometry -> problems with definitions
+/*#include <iostream>                  // inside .h geodesic_algorithm files
+#include <fstream>
+#include <string>
+*//*
+#include "geodesic_algorithm_dijkstra.h"
+#include "geodesic_algorithm_subdivision.h"
+#include "geodesic_algorithm_exact.h"*/
+///// ---- geodesic_algorithm
+
+///// interface.h  ---
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ** random function ran2() --
+#define IM1 2147483563
+#define IM2 2147483399
+#define AM (1.0/IM1)
+#define IMM1 (IM1-1)
+#define IA1 40014
+#define IA2 40692
+#define IQ1 53668
+#define IQ2 52774
+#define IR1 12211
+#define IR2 3791
+#define NTAB 32
+#define NDIV (1+IMM1/NTAB)
+#define RNMX (1.0-EPS)
+//**/ --- random ran2()
+
+#define PI 3.14159265359
+#define EPS 1.2e-7
+//#define DT_tolerance 0.45           // Fix tolerance for time-step estimation
+#define LINESIZE 130                // max amount of characters read "on a line"
+#define MAX_SIZE 180000             // max number of vertices, triangles on the mesh
+#define MAX_NEIGHBORS 500
+///// ---- interface.h
+
+#define OBTUSE_tolerance 30.        // allows only this % of obtuse angles in mesh
+#define TIME_DATA_POINTS 1000       //  hi.dat save this amount of time info points
+#define FIELD_CONF_FILES 10         // save this amount of s-t*dat configuration files
+#define DIVERGENCE_STOP 1e60        // consider the integration algorithm to diverge if energy reaches this value
+
+//#define VORTEX_EXP 2.               // in dislocation density approximation
+//#define VORTEX_COS 6.               // in dislocation density approximation
+
+#define  sign(x) ((x < 0) ? -1 : 1)
+#define  min(x,y) ((x < y) ? x : y)
+#define  max(x,y) ((x > y) ? x : y)
+#define  mod(x,y) ((x % y + y) % y)
+
+#define INPUT_FILE input_parameters.txt
+
+/*******************************************************/
+
+// *** Structure declarations  **
+
+typedef struct {
+
+	int label;
+
+	long num_of_neighbors;
+	long neighbor[MAX_NEIGHBORS];  //list of the vertex number of the neighboring vertices
+
+  	double x;
+	double y;
+	double z;
+	double r;
+
+	double hx;
+	double hy;
+	double hz;
+	double h2;
+	double kg;
+
+	double nx;
+	double ny;
+	double nz;
+
+	double phi;
+
+	double area;
+	double weight[MAX_NEIGHBORS];
+
+	double S;       // screened disclinicity from defect deformation
+	double vorticity;   // dislocation density **+++ =0.0 if only disloc-screened charge
+	double distToDefect[50];
+
+} Vertex;
+
+typedef struct {
+
+	long v1;
+	long v2;
+	long v3;
+
+} Triangle;
+
+typedef struct {
+
+	double x;
+	double y;
+	double z;
+
+	double q;
+	float qOff;
+
+	double core_r;
+	long closest_v;
+	//long num_of_connections;
+    //long connection[MAX_CONNECTIONS];
+
+} Defect;
+
+typedef struct {
+
+  	long d;
+  	long h;
+  	long m;
+  	long s;
+
+} CPU_Time;
+
+/*******************************************************/
+
+// *** Mesh variables used throughout the code **
+//extern long num_of_meshpoint;
+extern Vertex vertex[MAX_SIZE];
+extern double total_area;
+extern double total_volume;
+extern double dl_min;
+
+/*******************************************************/
+
+// *** File variables used throughout the code **
+extern FILE *f_ou;
+
+
+/*******************************************************/
+
+// *** Integration parameters read from input **
+extern double init_stress;          // initial total pre-stress
+extern double phi_range;            // set the initial stress at each point randomly within this range
+extern long seed;                   // for random fx to set initial stress value
+//extern long num_of_iteration;
+//extern double run_time;             // integration time step set accordingly  // change to read DT**
+extern double time_step;            // integration time step
+extern int integration_method;      // 1. Euler   2. Runge-Kutta (2 steps)  3. Runge-Kutta (4 steps) -> [bug]
+extern double convergence_stop;     // stop integration when num_iterations completed or reaching this error
+
+/*******************************************************/
+
+// *** integration variables used throughout the code **
+extern double convergence;
+
+/*******************************************************/
+// ***** main functions **
+void init_geometry();                       // 3     in: init_geometry.cpp
+//void run_field_integration();               // 5     in: run_field_integration.cpp
+//void end(time_t, time_t);                   // 6     in: common.cpp
+void get_time(time_t, CPU_Time *);          // 6.1   in: common.cpp
+
+// **** Common functions   **
+void export_conf(long, long);                               // 5.2.2
+void progress_bar(long, long);                              // 3.6.3
+int progress_bar_on_ou(long, long, int);                    // 3.6.3b
+
+
+
+#endif // COMMON_H_INCLUDED
